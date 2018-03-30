5f964ce94bbf8a529867ffceb1c006362b3cb5e3
Ranihilator <ranihilator@gmail.com>
2018-03-30 21:08:00 +0500

    new hw

---
+++ build/hw_03/CMakeFiles/allocator_test.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/Store/project/projects_for_home_work/hw_03/test/test.cpp" "/media/Store/project/projects_for_home_work/build/hw_03/CMakeFiles/allocator_test.dir/test/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_TEST_DYN_LINK"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../hw_03/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/media/Store/project/projects_for_home_work/build/hw_03/CMakeFiles/_alloc.dir/DependInfo.cmake"
+  "/media/Store/project/projects_for_home_work/build/hw_03/CMakeFiles/_manager.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/hw_03/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /media/Store/project/projects_for_home_work/hw_03
+# Build directory: /media/Store/project/projects_for_home_work/build/hw_03
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(allocator_test "allocator_test")
+add_test(container_test "allocator_test")
+add_test(task_test "allocator_test")
