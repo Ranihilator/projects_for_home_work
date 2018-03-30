5a7ccb06ef37446ff3ea412eb0dc00b68aad6362
Ranihilator <ranihilator@gmail.com>
2018-03-30 23:36:53 +0500

    travis config script

--- .travis_script.sh
+++ .travis_script.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-set -euo pipefail
 
 ACTION="${1}"
 
-if [ ${ACTION} == 'build' ]
-then
+if [[ ${ACTION} == 'build' ]] then;
     echo 'build...'
     
     mkdir dist hw_01/build hw_02/build hw_03/build
@@ -26,8 +24,7 @@ then
     cat hw_02/ip.tsv | hw_02/build/ip_filter > hw_02/out.tsv
     md5sum hw_02/out.tsv
     
-elif [ ${ACTION} == 'deploy' ]
-then
+elif [[ ${ACTION} == 'deploy' ]] then;
     echo 'Deploying...'
     
     curl -T dist/helloworld-1.0.$TRAVIS_BUILD_NUMBER-Linux.deb -u $BINTRAY_USER:$BINTRAY_API_KEY -H "X-Bintray-Debian-Distribution:trusty" -H "X-Bintray-Debian-Component:main" -H "X-Bintray-Debian-Architecture:amd64" https://api.bintray.com/content/$BINTRAY_USER/Cpp_Course/helloworld/$TRAVIS_BUILD_NUMBER/pool/h/helloworld/helloworld-1.0.$TRAVIS_BUILD_NUMBER-Linux.deb;deb_distribution=trusty;deb_component=main;deb_architecture=amd64;publish=1
