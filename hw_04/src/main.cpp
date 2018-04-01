d52a3b8d1bfb98781f25166fa7e00fe780cc0713
Ranihilator <ranihilator@gmail.com>
2018-04-01 15:26:13 +0500

    hw_04 begin

--- hw_04/src/main.cpp
+++ hw_04/src/main.cpp
@@ -1,7 +1,21 @@
 #include "main.h"
+using namespace PRINT;
 
 int main()
 {
-    
+    std::tuple<int,int,int,int> a{1,2,3,4};
+
+    print_ip(static_cast<char>(-1));
+    print_ip(static_cast<short>(0));
+    print_ip(static_cast<int>(2130706433));
+    print_ip(static_cast<long>(8875824491850138409));
+
+    print_ip(std::vector<char>{1,2,3,4});
+    print_ip(std::list<short>{5,6,7,8});
+
+    print_ip(std::string("1.2.3.4"));
+
+    print_ip(std::make_tuple(1,2,3,4));
+
     return 0;
 }
--- hw_04/src/main.h
+++ hw_04/src/main.h
@@ -1,3 +1,87 @@
 #pragma once
 
+#include <iostream>
+#include <sstream>
+#include <list>
+#include <vector>
+#include <deque>
+#include <tuple>
 
+namespace PRINT
+{
+
+using std::cout;
+using std::endl;
+using std::enable_if_t;
+using std::is_arithmetic;
+using std::is_same;
+using std::stringstream;
+using std::string;
+using std::vector;
+using std::list;
+
+
+///-------------------------value---------------------------------------------------///
+template <class T>
+enable_if_t<is_arithmetic<T>::value,void> print_ip (T &&arg)
+{
+    uint8_t size = sizeof(T);
+    uint8_t *data = (uint8_t*)&arg;
+
+    stringstream str;
+
+    for (auto i = size-1; i >= 0; --i)
+    {
+		if (str.tellp() != 0)
+			str << ".";
+
+        str << static_cast<int64_t>(data[i]);
+    }
+
+    cout << str.str() << endl;
+}
+
+///-------------------------string---------------------------------------------------///
+template <class T>
+enable_if_t<is_same<T, string>::value,void> print_ip (T &&arg)
+{
+    cout << arg << endl;
+}
+
+
+///-------------------------container---------------------------------------------------///
+template <class T>       struct is_stl_container:std::false_type{};
+template <class... Args> struct is_stl_container<std::vector            <Args...>>:std::true_type{};
+template <class... Args> struct is_stl_container<std::list              <Args...>>:std::true_type{};
+
+template <class T>
+enable_if_t<is_stl_container<T>::value,void> print_ip (T &&arg)
+{
+    stringstream str;
+
+    for (const auto &i : arg)
+    {
+        if (str.tellp() != 0)
+			str << "..";
+
+        str << static_cast<int64_t>(i);
+    }
+    cout << str.str() << endl;
+}
+
+
+
+///-------------------------tuple---------------------------------------------------///
+template<typename Test, template<typename...> class Ref>
+struct is_specialization : std::false_type {};
+
+template<template<typename...> class Ref, typename... Args>
+struct is_specialization<Ref<Args...>, Ref>: std::true_type {};
+
+template <class T>
+enable_if_t<is_specialization<T, std::tuple>::value,void> print_ip (T &&arg)
+{
+    std::cout << "tuple" << std::endl;
+}
+
+}
