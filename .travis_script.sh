0df873ed1dfe6e24c07127dae282a212766469a3
Ranihilator <ranihilator@gmail.com>
2018-04-04 19:50:38 +0500

    fix

--- .travis_script.sh
+++ .travis_script.sh
@@ -10,13 +10,11 @@ if [[ ${ACTION} == 'build' ]]; then
     cmake -H. -Bbuild
     cmake --build build
     cmake --build build --target 
-    cmake --build build --target package
+    cmake --build build --target package    
     
     sh -c 'cd build/hw_01 && ctest -V'
     sh -c 'cd build/hw_02 && ctest -V'
     sh -c 'cd build/hw_03 && ctest -V'
-    
-    mv build/*.deb dist/
 
     cat hw_02/ip.tsv | build/hw_02/ip_filter > hw_02/out.tsv
     md5sum hw_02/out.tsv
@@ -28,7 +26,7 @@ elif [[ ${ACTION} == 'deploy' ]]; then
 
     echo "Deploying..."
 
-    curl -T dist/home_work-1.0.$TRAVIS_BUILD_NUMBER-amd64-helloworld.deb -u $BINTRAY_USER:$BINTRAY_API_KEY -H "X-Bintray-Debian-Distribution:trusty" -H "X-Bintray-Debian-Component:main" -H "X-Bintray-Debian-Architecture:amd64" https://api.bintray.com/content/$BINTRAY_USER/projects_for_home_work/helloworld/$TRAVIS_BUILD_NUMBER/pool/h/helloworld/helloworld-1.0.$TRAVIS_BUILD_NUMBER-Linux.deb;deb_distribution=trusty;deb_component=main;deb_architecture=amd64;publish=1
+    curl -T dist/home_work-1.0.$TRAVIS_BUILD_NUMBER-amd64-helloworld.deb -u $BINTRAY_USER:$BINTRAY_API_KEY -H "X-Bintray-Debian-Distribution:trusty" -H "X-Bintray-Debian-Component:main" -H "X-Bintray-Debian-Architecture:amd64" https://api.bintray.com/content/$BINTRAY_USER/projects_for_home_work/helloworld/1.0/pool/h/helloworld/helloworld-1.0.$TRAVIS_BUILD_NUMBER-Linux.deb;deb_distribution=trusty;deb_component=main;deb_architecture=amd64;publish=1
     curl -X POST -u $BINTRAY_USER:$BINTRAY_API_KEY https://api.bintray.com/content/$BINTRAY_USER/projects_for_home_work/helloworld/$TRAVIS_BUILD_NUMBER/publish
 
     curl -T dist/home_work-1.0.$TRAVIS_BUILD_NUMBER-amd64-ip_filter.deb -u $BINTRAY_USER:$BINTRAY_API_KEY -H "X-Bintray-Debian-Distribution:trusty" -H "X-Bintray-Debian-Component:main" -H "X-Bintray-Debian-Architecture:amd64" https://api.bintray.com/content/$BINTRAY_USER/projects_for_home_work/ip_filter/1.0/pool/i/ip_filter/ip_filter-1.0.$TRAVIS_BUILD_NUMBER-Linux.deb;deb_distribution=trusty;deb_component=main;deb_architecture=amd64;publish=1
