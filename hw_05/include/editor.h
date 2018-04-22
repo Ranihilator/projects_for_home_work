a29e95293326eb6824b8813c80b273b5ba773ed7
Egor Rasskazov <ranihilator@gmail.com>
2018-04-22 22:14:04 +0500

    format

--- hw_05/include/editor.h
+++ hw_05/include/editor.h
@@ -1,1 +1,28 @@
- 
+#pragma once
+
+#include <iostream>
+#include "format.h"
+
+/// Home work #5 (simple uml)
+namespace HW_05
+{
+
+class Editor
+{
+
+public:
+    void Create_Document()
+    {}
+
+    void Close_Document()
+    {}
+
+    void Import_Document()
+    {}
+
+    void Export_Document()
+    {}
+
+};
+
+}
--- hw_05/src/main.cpp
+++ hw_05/src/main.cpp
@@ -1,1 +1,43 @@
- 
+/*!
+\file
+\brief Create UML diagram from simple vector graph editor
+
+Simple UML design functional:
+Create new documents
+Import documents from file
+Export documents to file
+Create simple graph elements
+*/
+
+#include "version_library.h"
+#include "editor.h"
+
+using namespace HW_05;
+
+/*!
+\brief Start program
+
+\param[in] argc argument's count
+\param[in] argv array of arguments
+\return process code
+*/
+int main(int argc, char* argv[])
+{
+	if (argc > 1)
+	{
+		if (std::string(argv[1]) == "-v")
+			HW_01::version();
+		else
+		{
+			std::cout << "Usage:" << std::endl;
+			std::cout << "	simple_uml without arguments to run" << std::endl;
+			std::cout << "	simple_uml -v Current version print" << std::endl;
+			std::cout << std::endl;
+		}
+
+
+
+		return 0;
+    }
+}
+
