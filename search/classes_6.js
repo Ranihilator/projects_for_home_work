4bac452bcb90778f38661eeded77596529090e51
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-28 17:02:00 +0000

    Deploy Ranihilator/projects_for_home_work to github.com/Ranihilator/projects_for_home_work.git:gh-pages

--- search/classes_6.js
+++ search/classes_6.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['line',['Line',['../d3/da0/classHW__05_1_1SHAPES_1_1Line.html',1,'HW_05::SHAPES']]],
-  ['loader',['Loader',['../df/d66/classHW__05_1_1Loader.html',1,'HW_05']]]
+  ['line',['Line',['../d3/da0/classHW__05_1_1SHAPES_1_1Line.html',1,'HW_05::SHAPES']]]
 ];
