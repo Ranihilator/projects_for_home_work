637b95e724ae003bcba67bfbff1bf7ebd87d96f6
Egor Rasskazov <ranihilator@gmail.com>
2018-08-14 23:36:45 +0500

    HW_06
    Start branch

---
+++ hw_06/src/main.cpp
@@ -0,0 +1,28 @@
+/*!
+\file
+\brief Create 2x - dimension sparse matrix
+
+Create 2x matrix
+Matrix have sparse elements
+Matrix can return size
+Matrix can iterrate all node
+Matrix can return default value from sparse space
+*/
+
+#include "version_library.h" 
+
+using namespace HW_06;
+
+#include <iostream>
+
+/*!
+\brief Start program
+
+\param[in] argc argument's count
+\param[in] argv array of arguments
+\return process code
+*/
+int main(int argc, char* argv[])
+{
+	return 0;
+}
